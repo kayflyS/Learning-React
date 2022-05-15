328d4a77e6484c7bee98a1038d32dc669475803a
caleb olusegun <olupeniel2000@gmail.com>
2022-05-15 23:45:52 +0100

    learning react

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,12 @@ function Header(props){
 
     return(
     <div>
+        <ch1 id="header">{props.name}</ch1>
         <h3 id="header">{props.name}</h3> 
         <p className="text">{props.author} </p>
     </div> 
+    
+    
    )
 
 }
