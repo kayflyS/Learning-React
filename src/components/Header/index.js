b078de8048978d8fc967d47294cfa8faa077e389
caleb olusegun <olupeniel2000@gmail.com>
2022-07-07 23:51:26 +0100

    changes made

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header(props){
 
     return(
     <div>
-        <ch1 id="header">{props.name}</ch1>
+        <h1 id="header">{props.name}</h1>
         <h3 id="header">{props.name}</h3> 
         <p className="text">{props.author} </p>
     </div> 
